3304342fb297b4d902d414f0d212ad6e4ca8c28c
Ed Brown <e_brown18@hotmail.co.uk>
2024-01-16 16:22:57 +0000

    Tweak adding github link and fixing padding

--- app/components/StickyLinks.tsx
+++ app/components/StickyLinks.tsx
@@ -4,7 +4,7 @@ export function StickyLinks() {
     return <div id="stickyLinks">
         <Link className="circleLink" href="https://www.facebook.com/edmond.brown.7" target="_blank">f</Link>
         <Link className="circleLink" id="link" href="https://uk.linkedin.com/in/edmund-brown-08a2892a/" target="_blank">in</Link>
-        {/* <Link className="circleLink" href="https://community.gamedev.tv/u/Brownie/summary" target="_blank">Ga</Link> */}
+        <Link className="circleLink" href="https://github.com/Jimbothemighty" target="_blank">gh</Link>
         <Link className="circleLink" href="mailto:ed@edmundbrown.com">✉️</Link>
     </div>
 }
